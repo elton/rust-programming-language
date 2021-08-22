50b17fa3f29b71f887b5dd9f61308946c79f3000
Elton Zheng <elton.zheng.sh@gmail.com>
2021-08-22 20:45:26 +0800

    update ch03.

---
+++ ch04/target/rls/debug/deps/ch04-0d1796231325548c.d
@@ -0,0 +1,5 @@
+/Volumes/data/study/Rust/rust-programming-laguage/ch04/target/rls/debug/deps/ch04-0d1796231325548c.rmeta: src/main.rs
+
+/Volumes/data/study/Rust/rust-programming-laguage/ch04/target/rls/debug/deps/ch04-0d1796231325548c.d: src/main.rs
+
+src/main.rs:
