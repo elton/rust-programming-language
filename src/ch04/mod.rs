f87821b0a7120ff092051ce70a326ff75f1d08b0
Elton Zheng <elton.zheng.sh@gmail.com>
2021-09-01 17:34:04 +0800

    update code in chapter 4.

--- src/ch04/mod.rs
+++ src/ch04/mod.rs
@@ -9,7 +9,7 @@ pub fn string_owner() {
   println!("{}", s);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
   let bytes = s.as_bytes();
 
   for (i, &item) in bytes.iter().enumerate() {
@@ -22,8 +22,28 @@ fn first_word(s: &String) -> &str {
 }
 
 pub fn find_1st_word() {
-  let s = String::from("Hello world!");
-  let word = first_word(&s);
+  let my_string = String::from("Hello world!");
 
-  println!("the first word is : {}", word);
+  // `first_word` works on slices of `String`s, whether partial or whole
+  let word = first_word(&my_string[0..6]);
+  println!("{}", word);
+  let word = first_word(&my_string[..]);
+  println!("{}", word);
+  // `first_word` also works on references to `String`s, which are equivalent
+  // to whole slices of `String`s
+  let word = first_word(&my_string);
+  println!("{}", word);
+
+  let my_string_literal = "hello world";
+
+  // `first_word` works on slices of string literals, whether partial or whole
+  let word = first_word(&my_string_literal[0..6]);
+  println!("{}", word);
+  let word = first_word(&my_string_literal[..]);
+  println!("{}", word);
+
+  // Because string literals *are* string slices already,
+  // this works too, without the slice syntax!
+  let word = first_word(my_string_literal);
+  println!("{}", word);
 }
