b3e8f48b0c58a601ba867974716b33665bcecdac
Elton Zheng <elton.zheng.sh@gmail.com>
2021-09-28 22:00:41 +0800

    update the functions of chapter 6.

--- src/ch06/mod.rs
+++ src/ch06/mod.rs
@@ -12,12 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// you can put any kind of data inside an enum variant: strings, numeric types, or structs
+#[derive(Debug)]
 enum IpAddr {
   V4(String),
   V6(String),
 }
-pub fn ipAddress() {
+
+impl IpAddr {
+  fn call(&self) {
+    println!("Home IP IpAddr:{:?}", self);
+  }
+}
+
+pub fn ip_address() {
   let home = IpAddr::V4(String::from("127.0.0.1"));
+  home.call();
 
   let loopback = IpAddr::V6(String::from("::1"));
+  loopback.call();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod ch03;
 mod ch04;
 mod ch05;
+mod ch06;
 
 fn main() {
     ch03::basic_type();
@@ -15,4 +16,5 @@ fn main() {
     ch04::slices();
 
     ch05::calc_area();
+    ch06::ip_address();
 }
